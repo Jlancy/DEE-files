a2bd5d39dca0bbdb9f294db098c2be0a8afb1a26
PhilTanw <philiptanwongprasert@gmail.com>
2015-07-13 14:49:27 -0700

    Another Build
    Rename folder to KTGroundZeroBuild
    + Added Game Manager (to be implemented and used)
    + Added boundless player movement
    + Added an input for 'T' for the player to do an emote (this will turn
    into attack)
    + Added camera script to follow the player

---
+++ KTGroundZeroBuild/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float cameraSpeed = 0.1f;
+	Camera cam;
+
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Normalize resolution
+		//Height / 100.0(to float) / scale facter
+		cam.orthographicSize = (Screen.height / 100f / 4f);
+		if (target) {
+			//from, to, speed
+			//add 0, 0, 10 to keep distance to view the scene
+			transform.position = Vector3.Lerp(target.position, target.position, cameraSpeed) + new Vector3(0, 0, -10);
+		}
+	}
+}
---
+++ KTGroundZeroBuild/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	Rigidbody2D rBody;
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		rBody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 movementVector = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+	
+		if (movementVector != Vector2.zero) {
+			anim.SetBool ("isWalking", true);
+			anim.SetFloat ("inputX", movementVector.x);
+			anim.SetFloat ("inputY", movementVector.y);
+		} 
+		else
+			anim.SetBool ("isWalking", false);
+
+		if(Input.GetKey(KeyCode.T))
+			anim.SetBool ("tIsPress", true);
+		else
+		   	anim.SetBool ("tIsPress", false);
+
+		rBody.MovePosition (rBody.position + movementVector * Time.deltaTime);
+	}
+}
